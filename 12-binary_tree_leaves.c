bea87acf840940a4657851c52c9d1a89e99605c6
Nour <nourtaktak280@gmail.com>
2020-07-22 16:56:02 +0100

    fix

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,4 +1,9 @@
 #include "binary_trees.h"
+/**
+ * binary_tree_leaves - function that counts the leaves in a binary tree
+ * @tree: a pointer to the parent node of the node.
+ * Return: the leaves of the tree or 0.
+ */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
 	if (!tree)
