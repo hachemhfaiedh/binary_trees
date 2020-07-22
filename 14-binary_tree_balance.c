9e394c6ee1c4b117505240c091753d7cb5366adf
hachemhfaiedh <hechemhfaiedh@gmail.com>
2020-07-22 18:00:24 +0000

    balance

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,13 +1,13 @@
 #include "binary_trees.h"
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int left_height = 0, right_height = 0;
+	int left_height, right_height;
 
-	if (!tree || (!tree->left && !tree->right))
+	if (!tree)
 		return (0);
 	left_height= binary_tree_height(tree->left);
 	right_height = binary_tree_height(tree->right);
-	if (left_height >= right_height)
+	if (left_height > right_height)
 		return (left_height + 1);
 	else
 		return (right_height + 1);
@@ -16,14 +16,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
 #include "binary_trees.h"
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int l_height = 0, r_height = 0;
+	int l_height, r_height;
 
 	if (!tree)
 		return (0);
-	if (!tree->right && tree->left)
-		return (1);
-	if (tree->right && !tree->left)
-		return (-1);
 	l_height = binary_tree_height(tree->left);
 	r_height = binary_tree_height(tree->right);
 	return (l_height - r_height);
