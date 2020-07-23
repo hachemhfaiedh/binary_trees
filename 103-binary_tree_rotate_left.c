7573636334df73782cc0e8d493fcefd60e1e6b14
Nour <nourtaktak280@gmail.com>
2020-07-23 13:32:53 +0100

    fix

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -4,7 +4,7 @@
  * @tree: tree to be rotated
  * Return:  pointer to the new root node of the tree once rotated
  */
-binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree);
+binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	if (!tree)
 	return (NULL);
